544b56b405ecf1e0ff75aca991b7ef3ecd3bcb95
Charles Loxton <charlesloxtonf@gmail.com>
2023-02-06 16:48:20 +0200

    Add project files.

---
+++ APICaller/APIRequests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICaller
+{
+    public class APIRequests
+    {
+        private HttpClient client;
+        public APIRequests()
+        {
+            client = new HttpClient();
+        }
+        public Task<HttpResponseMessage> AddClient(string name, string guid)
+        {
+            string requestBody = "{\"name\": \"" + name + "\", \"guid\": \"" + guid + "\"}";
+
+            HttpContent firstRequestContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            Task<HttpResponseMessage> firstResponseTask = client.PostAsync("https://localhost:7286/api/Client/Create", firstRequestContent);
+
+            return firstResponseTask;
+        }
+        public Task<HttpResponseMessage> UpdateClient(string id, string name, string userid, string guid)
+        {
+            string requestBody = "{\"id\": \"" + id + "\", \"name\": \"" + name + "\", \"userid\": \"" + userid + "\", \"guid\": \"" + guid + "\"}";
+
+            HttpContent firstRequestContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            Task<HttpResponseMessage> firstResponseTask = client.PutAsync("https://localhost:7286/api/Client/Update", firstRequestContent);
+
+            return firstResponseTask;
+        }
+
+    }
+}
---
+++ APICaller/Program.cs
@@ -0,0 +1,23 @@
+﻿using APICaller;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var api = new APIRequests();
+
+//var response1 = api.AddClient("TestClient1", "63fdsf87sd8f7sd8");
+//var response2 = api.AddClient("TestClient2", "53fdsf87sd8f7sd8");
+
+//This makes two Update calls to the api at the same time, the one with the smallest payload will execute first
+var response1 = api.UpdateClient("17", "TestClient3", "123", "sfdudshfsdfsd7fs");
+var response2 = api.UpdateClient("17", "TestClient4", "123", "dfdfdsfsdfsd");
+
+// Wait for both requests to complete
+await Task.WhenAll(response1, response2);
+
+// Write out the responses
+Console.WriteLine("First request response: " + await response1.Result.Content.ReadAsStringAsync());
+Console.WriteLine("Second request response: " + await response2.Result.Content.ReadAsStringAsync());
+
+Console.WriteLine("Both requests completed");
+Console.ReadLine();
